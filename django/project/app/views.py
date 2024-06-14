596c6fbaddd2ad3f0a16531d90ab65a6ebf15b05
TaiChi <anothai.0978452316@gmail.com>
2024-06-14 22:26:33 +0700

    Edit : rendering home.html

--- django/project/app/views.py
+++ django/project/app/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
 
-def index(request):
-    return render(request,'app/index.html')
+# Create your views here.
+def sign_up(request):
+    return render(request, 'app/sign_up.html')
 
 def sign_in(request):
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-    return render(request,'app/sign_in.html')
+    return render(request, 'app/sign_in.html')
+
+def index(request):
+    return render(request, "app/index.html")
+def home(request):
+    return render(request, "app/home.html")
 
 def product_list(request):
-    return render(request,'app/product_list.html')
+    return render(request, "app/product_list.html")
+
 
 def product_detail(request):
-    return render(request,'app/product_detail.html')
+    return render(request, "app/product_detail.html")
