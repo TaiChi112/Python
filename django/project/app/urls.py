3475ccfc730f9545f9b95aaff836dea0430ced92
TaiChi <anothai.0978452316@gmail.com>
2024-06-14 22:25:33 +0700

    Edit : adding path url for home pages

--- django/project/app/urls.py
+++ django/project/app/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('sign_in/',views.sign_in,name='sign_in'),
-    path('product_list/',views.product_list,name='product_list'),
-    path('productList/productDetail/',views.product_detail,name='product_detail'),
+    path("home/", views.index, name="index"),
+    path("", views.home, name="home"),
+    path("sign_up/", views.sign_up, name="sign_up"),
+    path("sign_in/", views.sign_in, name="sign_in"),
+    path("product_list/", views.product_list, name="product_list"),
+    path("product_list/product_detail/", views.product_detail, name="product_detail"),
 ]
