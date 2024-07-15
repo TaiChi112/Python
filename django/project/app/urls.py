30625748d091057318d4956549f2e3bef0d7e45d
TaiChi <anothai.0978452316@gmail.com>
2024-07-15 20:33:21 +0700

    chore : set new url when our click

--- django/project/app/urls.py
+++ django/project/app/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 urlpatterns = [
     path("home/", views.index, name="index"),
+    path("profile/", views.profile, name="profile"),
     path("", views.home, name="home"),
     path("sign_up/", views.sign_up, name="sign_up"),
     path("sign_in/", views.sign_in, name="sign_in"),
     path("product_list/", views.product_list, name="product_list"),
-    path("product_list/product_detail/", views.product_detail, name="product_detail"),
+    path("product_list/product_detail/<int:id>/", views.product_detail, name="product_detail"),
+    path("test/<int:id>/<str:name>/", views.test, name="test"),
 ]
