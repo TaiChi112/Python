b35d86ab3d94d6e4971dceb8d001298b57861abf
TaiChi <anothai.0978452316@gmail.com>
2024-08-11 00:35:15 +0700

    fix : edit data mock add new field

--- django/project_django2/product_app/mock.py
+++ django/project_django2/product_app/mock.py
@@ -1,5 +1,5 @@
 product_list = [
-    {'id': 1, 'name': 'mama', 'price': 7},
-    {'id': 2, 'name': 'oil', 'price': 35},
-    {'id': 3, 'name': 'milk', 'price': 15},
+    {'id': 1, 'name': 'Product 1', 'description': 'Description of Product 1', 'price': 100},
+    {'id': 2, 'name': 'Product 2', 'description': 'Description of Product 2', 'price': 200},
+    {'id': 3, 'name': 'Product 3', 'description': 'Description of Product 3', 'price': 300},
 ]
