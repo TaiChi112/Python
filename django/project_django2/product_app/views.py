6fa80bd35cdb80575914144fbcea42b7f5e7eed5
TaiChi <anothai.0978452316@gmail.com>
2024-08-11 00:33:55 +0700

    fix : adding condition for find product specific

--- django/project_django2/product_app/views.py
+++ django/project_django2/product_app/views.py
@@ -6,5 +6,11 @@ from .mock import product_list
 def products(request):
     return render(request, 'product_app/products.html',{'product_list': product_list})
 
-def product(request,data):
-    return render(request, 'product_app/product.html',{'data':data})
+def product_detail(request, id):
+    product = next((prod for prod in product_list if prod['id'] == id), None)
+    # print(product)
+    context = {
+        'product': product
+    }
+    # print(context)
+    return render(request, 'product_app/product.html', context)
