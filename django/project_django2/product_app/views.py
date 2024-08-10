79d63c2ca17870c6d5461618a01a24dd1c66c9f9
TaiChi <anothai.0978452316@gmail.com>
2024-08-10 23:19:44 +0700

    feat : define fn rendering page following by templates

--- django/project_django2/product_app/views.py
+++ django/project_django2/product_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .mock import product_list
 
 # Create your views here.
+
+def products(request):
+    return render(request, 'product_app/products.html',{'product_list': product_list})
+
+def product(request,data):
+    return render(request, 'product_app/product.html',{'data':data})
