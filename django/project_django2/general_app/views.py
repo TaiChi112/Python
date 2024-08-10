86a7456744f3d7b121b7dc871b41cb62755c0fbf
TaiChi <anothai.0978452316@gmail.com>
2024-08-10 23:19:15 +0700

    feat : define fn rendering page following by templates

--- django/project_django2/general_app/views.py
+++ django/project_django2/general_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'general_app/home.html')
+
+def about(request):
+    return render(request, 'general_app/about.html')
