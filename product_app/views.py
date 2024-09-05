36b22a6e64ff3732c8d63f29af26b85398f875eb
TaiChi <anothai.0978452316@gmail.com>
2024-09-05 17:42:29 +0700

    chore : import data mock render in template

--- product_app/views.py
+++ product_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .mock import product_mock
 
 # Create your views here.
 
 def Product_view(request):
-    return render(request, 'product/products.html')
+    context = {
+        'products': product_mock
+    }
+    return render(request, 'product/products.html', context)
