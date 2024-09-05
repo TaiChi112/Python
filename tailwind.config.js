46a6dad2fba8c5fa481208f7d147a542f6a4101c
TaiChi <anothai.0978452316@gmail.com>
2024-09-05 23:28:43 +0700

    config : content tailwindcss for can use it, in the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './templates/**/*.html',
+    './static/**/*.js',
+    './static/**/*.css',
+    "./general_app/templates/**/*.html",
+    "./product_app/templates/**/*.html",
+  ],
   theme: {
     extend: {},
   },
