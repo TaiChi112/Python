9631fae41d6b7b3ef092446a1a55aa0887221fb5
TaiChi <anothai.0978452316@gmail.com>
2024-09-05 19:54:11 +0700

    refactor : change DB to mysql & config a little bit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -78,9 +78,17 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecommerce',
+        'USER': 'root',
+        'PASSWORD': 'TaiChi',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
