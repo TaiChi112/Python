a1c92edb678009567cde8f15e283f5380ba361d1
TaiChi <anothai.0978452316@gmail.com>
2024-09-07 00:03:46 +0700

    fix : query data in DB swap with data mock

--- product_app/views.py
+++ product_app/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from .mock import product_mock
+from .models import Product
 
 # Create your views here.
 
 def Product_view(request):
+    # context = {
+    #     'products': product_mock
+    # }
+
     context = {
-        'products': product_mock
+        'products': Product.objects.all()
     }
     return render(request, 'product/products.html', context)
