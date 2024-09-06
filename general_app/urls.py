89a5e6183257148d46ae5749dc0af935c7cb985b
TaiChi <anothai.0978452316@gmail.com>
2024-09-06 22:50:10 +0700

    feat : new path sign_up

--- general_app/urls.py
+++ general_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('sign_in/', views.sign_in, name='sign_in')
+    path('sign_in/', views.sign_in, name='sign_in'),
+    path('sign_up/', views.sign_up, name='sign_up'),
 ]
