6a0062936580b93d74306212e092dde5c142aba8
TaiChi <anothai.0978452316@gmail.com>
2024-09-06 23:08:16 +0700

    fix : adding field image url

--- product_app/models.py
+++ product_app/models.py
@@ -4,9 +4,10 @@ from django.db import models
 
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    price = models.FloatField()
-    description = models.TextField()
+    name = models.CharField(max_length=100,null=True)
+    price = models.FloatField(null=True)
+    description = models.TextField(null=True)
+    image_url = models.URLField(null=True)
 
     class Meta:
         db_table = 'product'
