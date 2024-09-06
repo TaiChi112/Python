c399c0dc3ecb3e9b6ffd658e5f0ffa4cf0f3ee3a
TaiChi <anothai.0978452316@gmail.com>
2024-09-07 01:43:55 +0700

    chore : I forgot What I change?

--- general_app/views.py
+++ general_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import CustomUser
 from .forms import SignUpForm, SignInForm
 from django.contrib.auth import authenticate, login
