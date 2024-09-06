3a08fbfc2756fd2d417b2285c40df70c2432fe13
TaiChi <anothai.0978452316@gmail.com>
2024-09-07 01:01:28 +0700

    fix : manage function sign up & sign in

--- general_app/views.py
+++ general_app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import CustomUser
+from .forms import SignUpForm, SignInForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -8,8 +11,38 @@ def index(request):
 def about(request):
     return render(request, 'pages/about.html')
 
-def sign_in(request):
-    return render(request, 'account/sign_in.html')
-
 def sign_up(request):
-    return render(request, 'account/sign_up.html')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    
+    context = {
+        'sign_up': form
+    }
+    return render(request, 'account/sign_up.html',context)
+
+def sign_in(request):
+    if request.method == 'POST':
+        form = SignInForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(email=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+            else:
+                form.add_error(None, 'Invalid email or password')
+    else:
+        form = SignInForm()
+
+    context = {
+        'sign_in': form}
+    return render(request, 'account/sign_in.html',context)
