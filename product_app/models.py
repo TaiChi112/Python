b7cef706f12817e664e26122ce7d41b6f1b052e8
TaiChi <anothai.0978452316@gmail.com>
2024-09-08 01:25:02 +0700

    feat : new to model Cart & Cart item

--- product_app/models.py
+++ product_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from general_app.models import CustomUser
 
 # Create your models here.
 
@@ -11,3 +12,24 @@ class Product(models.Model):
 
     class Meta:
         db_table = 'product'
+
+class Cart(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE,null=True,blank=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'cart'
+    
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def get_total_price(self):
+        return self.quantity * self.product.price
+    
+    # def get_total_price_pices(self):
+    #     return self.quantity
+    class Meta:
+        db_table = 'cart_item'
