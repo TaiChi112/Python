e284ecca336df7edf50db7519a04e5c4e75ccb6a
TaiChi <anothai.0978452316@gmail.com>
2024-09-08 01:22:22 +0700

    feat : fn add_to_cart, cart view & another condition fn

--- product_app/views.py
+++ product_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .mock import product_mock
-from .models import Product
+from .models import Product, Cart, CartItem
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
 
@@ -24,3 +26,33 @@ def product_view(request, id):
         'product': product
     }
     return render(request, 'product/product.html', context)
+
+def cart_view(request):
+     if request.user.is_authenticated:
+        # ถ้า user มี cart แล้ว ให้ดึงมาใช้ แต่ถ้าไม่มีให้สร้างใหม่
+        cart, created = Cart.objects.get_or_create(user=request.user)
+        cart_items = CartItem.objects.filter(cart=cart)
+     else:
+        cart_items = []  # กรณีที่ผู้ใช้ยังไม่ล็อกอิน
+
+     total_price = sum(item.get_total_price() for item in cart_items)
+
+     context = {
+        'cart_items': cart_items,
+        'total_price': total_price,
+     }
+     return render(request, 'cart/cart.html',context)
+
+def add_to_cart(request, id):
+    product = get_object_or_404(Product, id=id)
+
+    if request.user.is_authenticated:
+        cart, created = Cart.objects.get_or_create(user=request.user)
+    
+    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+
+    if not created:
+        cart_item.quantity += 1
+    cart_item.save()
+
+    return redirect('cart')
