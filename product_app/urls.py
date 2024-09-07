84b8f45313ba7d67d08241eb1e6d92dce86a1c89
TaiChi <anothai.0978452316@gmail.com>
2024-09-08 01:23:02 +0700

    feat : new path cart, add to cart fn

--- product_app/urls.py
+++ product_app/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.products_view, name='products'),
     path('<int:id>/', views.product_view, name='product'),
+    path('cart/', views.cart_view, name='cart'),
+    path('add_to_cart/<int:id>/', views.add_to_cart, name='add_to_cart'),
 ]
