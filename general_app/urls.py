3b369bb86a33e0a2e22c55ca21fdc577cc89e38c
TaiChi <anothai.0978452316@gmail.com>
2024-09-08 01:26:14 +0700

    feat : new 2 path : sign out & profile

--- general_app/urls.py
+++ general_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('sign_in/', views.sign_in, name='sign_in'),
     path('sign_up/', views.sign_up, name='sign_up'),
+    path('sign_out/', views.sign_out, name='sign_out'),
+    path('profile', views.profile, name='profile'),
 ]
