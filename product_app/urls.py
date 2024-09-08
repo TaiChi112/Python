2b7443072ad19f9f54c319d0f78538f4d8983536
TaiChi <anothai.0978452316@gmail.com>
2024-09-08 16:09:02 +0700

    chore : forgot What I change

--- product_app/urls.py
+++ product_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:id>/', views.product_view, name='product'),
     path('cart/', views.cart_view, name='cart'),
     path('add_to_cart/<int:id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove_from_cart/<int:id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
