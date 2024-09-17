7c9ef13172dd0fae0213a8c25294b96c002cf599
TaiChi <anothai.0978452316@gmail.com>
2024-09-17 21:51:45 +0700

    chore : I delete this repo on github

--- general_app/forms.py
+++ general_app/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from .models import CustomUser
 
 class SignUpForm(forms.ModelForm):
-    # hiding password field
     password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
