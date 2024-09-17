4467ccbbb759d36c223f57f4d8a38de2d4ea1668
TaiChi <anothai.0978452316@gmail.com>
2024-09-17 20:06:49 +0700

    chore : this is wrong commit project error

--- django/project_django2/general_app/forms.py
+++ django/project_django2/general_app/forms.py
@@ -1,44 +1,25 @@
 from django import forms
-from .models import CustomUser, SingUpIUser, SignInIUser
-from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser    
 
-# class RegistrationForm(UserCreationForm):
+# class RegistrationForm(forms.ModelForm):
+#     password2 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
 #     class Meta:
 #         model = CustomUser
-#         fields = ['username', 'email', 'password1', 'password2']
-from django import forms
-from .models import CustomUser
+#         fields = ['username', 'email', 'password']
 
-class RegistrationForm(forms.ModelForm):
-    password2 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+#     def clean(self):
+#         cleaned_data = super().clean()
+#         password = cleaned_data.get("password")
+#         password2 = cleaned_data.get("password2")
 
-    class Meta:
-        model = CustomUser
-        fields = ['username', 'email', 'password']
+#         if password != password2:
+#             raise forms.ValidationError("Passwords do not match.")
 
-    def clean(self):
-        cleaned_data = super().clean()
-        password = cleaned_data.get("password")
-        password2 = cleaned_data.get("password2")
-
-        if password != password2:
-            raise forms.ValidationError("Passwords do not match.")
-
-        return cleaned_data
+#         return cleaned_data
 
 
-class LoginForm(forms.Form):
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
+# class LoginForm(forms.Form):
+#     email = forms.EmailField()
+#     password = forms.CharField(widget=forms.PasswordInput)
 
-    # password = forms.CharField(widget=forms.PasswordInput)
-class SignUpForm(forms.ModelForm):
-    class Meta:
-        model = SingUpIUser
-        fields = ['username', 'email', 'password']
-
-class SignInIUser(forms.ModelForm):
-    class Meta:
-        model = SignInIUser
-        fields = ['email', 'password']
-    
