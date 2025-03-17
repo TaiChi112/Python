f7adff7644d3b9ca61d42a1db1f47363c185ee93
TaiChi <anothai.0978452316@gmail.com>
2025-03-18 00:15:05 +0700

    dev: how to declaretion variable

--- python_feat_o1/main.py
+++ python_feat_o1/main.py
@@ -1,5 +1,24 @@
+
+
 def main()->int:
-    print("Python do something!")
+    x = 112
+    print("x:",x)
+    x, y,z = "Hello", "World", "Python"
+    print("x:",x)
+    print(type(x))
+    print("y:",y)
+    print("z:",z)
+    
+    name1= name2=name3 = "TaiChi"
+    print("name:",name1)
+    print("name:",name2)
+    print("name:",name3)
+    
+    fruits = ["apple", "banana", "cherry"]
+    x, y, z = fruits
+    print("x:",x)
+    print("y:",y)
+    print("z:",z)
     return 0
 
 if __name__ == "__main__":
