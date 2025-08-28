26b338b104d1bc97ba8a7fe5762df166534b4b07
TaiChi <anothai.0978452316@gmail.com>
2025-08-29 01:40:33 +0700

    feat: enhance main function with additional print statements and myfunc definition

--- python_feat_o1/main.py
+++ python_feat_o1/main.py
@@ -1,6 +1,14 @@
-
-
+import class1
+x0 = "awesome"
+def myfunc():
+    print("Python is " + x0)
+    x = "fantastic"
 def main()->int:
+    print(class1.greet1("John"))
+    class1.greet2("John")
+    
+    myfunc()
+    
     x = 112
     print("x:",x)
     x, y,z = "Hello", "World", "Python"
@@ -19,6 +27,14 @@ def main()->int:
     print("x:",x)
     print("y:",y)
     print("z:",z)
+    
+    x = 5
+    y = 3
+    print("result:",x+y)
+    x = 1j
+    print(type(x))
+    x = 35e3
+    print(type(x))
     return 0
 
 if __name__ == "__main__":
